371034e163b8f27c97704f2dce4a86c5ee96d90c
isjang08 <47512795+isjang08@users.noreply.github.com>
2021-03-15 20:09:52 +0900

    Update Count Primes.cpp

--- Math/Count Primes.cpp
+++ Math/Count Primes.cpp
@@ -13,7 +13,7 @@ public:
         }           
         return output.size(); 
     }
-
+    // a * b = c로 나뉜다면 a < sqrt(c), b>sqrt(c)
     bool isPrime(int i) {
         for (int c = 2; c*c <= i; c++)
         {
@@ -25,13 +25,15 @@ public:
 };
 
 
-//
+// one pass
     int countPrimes(int n) {
         int count{0};
         vector<bool> vect(n, true); 
 
+        // 어차피 2 부터 셀꺼다 
         for (int i = 2; i * i < n; i++) {
             if (vect[i]) {
+                // i^2 + i = i(i+1) 이니까 =+ i 한것은 전부 prime number가 아님.
                 for (int j = i * i; j < n; j += i) {
                     vect[j] = false;
                 }
