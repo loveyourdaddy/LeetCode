46be59866cee6b1d1922d5ad7961c15bc22b9074
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2021-05-17 17:28:01 +0900

    Update Unique Paths.cpp

--- DynamicProgramming/Unique Paths.cpp
+++ DynamicProgramming/Unique Paths.cpp
@@ -14,3 +14,22 @@ public:
             return n*factorial(n-1);
     }
 };
+//DP
+//0ms 
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //int[][] area = new int[n][m];
+        int area[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i ==0 || j==0)
+                    area[i][j] =1;
+                else // i & j >= 1
+                    area[i][j]=area[i-1][j] + area[i][j-1];
+            }
+        }
+        return area[m-1][n-1];
+    }
+    
+};
