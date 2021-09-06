852f679d42765aa1a6c91e6a0e1cdb0a4490426e
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2021-09-06 15:26:42 +0900

    Update MaximumProductSubarray.cpp

--- DynamicProgramming/MaximumProductSubarray.cpp
+++ DynamicProgramming/MaximumProductSubarray.cpp
@@ -1,4 +1,4 @@
-// 
+// Brute force 
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
@@ -22,5 +22,33 @@ public:
         }
 
         return max;
+    }
+};
+
+// DP
+// Runtime: 8 ms
+// Memory Usage: 11.8 MB
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+		int maxProduct = nums[0];
+		int minProduct = nums[0];
+        int ans = nums[0];
+        int length = nums.size();
+        
+        for (int i = 1; i < length; i++) // begin 
+        {
+            int now = nums[i];
+            int minTmp = min({ now, now * maxProduct, now * minProduct });
+            int maxTmp = max({ now, now * maxProduct, now * minProduct });
+            minProduct = minTmp;
+			maxProduct = maxTmp;
+            
+            if (ans < maxProduct)
+                ans = maxProduct;
+        }
+
+        return ans;
     }
 };
